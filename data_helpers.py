15cafd7bf5f1b9fb85a234e06a2bcecb40c7524d
James Watson <jameswatson16@hotmail.com>
2023-10-03 15:43:48 +0100

    Half feature prep

--- data_helpers.py
+++ data_helpers.py
@@ -1,4 +1,13 @@
-def feature_prep():
+def feature_prep(dataset):
+    df = dataset[["whatdidyousee", "category2"]]
+
+    df = df.loc[df["category2"].notnull()]
+    df = df.reset_index(drop=True)
+
+    df["category"] = df["category2"].str.strip()
+    category_list = get_categories()
+
+    
     raise NotImplementedError()
 
 def split_sample():

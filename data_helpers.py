e34afefde207643f940acee38fb1e67ed49355e9
James Watson <jameswatson16@hotmail.com>
2023-10-05 10:42:38 +0100

    Custom Dataset getitem

--- data_helpers.py
+++ data_helpers.py
@@ -14,6 +14,31 @@ class CustomDataset:
         self.targets =  self.data.target_list
         self.max_len = max_len
 
+    def __getitem__(self, index):
+        input = str(self.input[index])
+        input = " ".join(input.split())
+
+        inputs = self.tokenizer.encode_plus(
+            input,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding="max_length",
+            return_token_type_ids=True,
+            truncation=True,
+        )
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+        token_type_ids = input["token_type_ids"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "targets": torch.tensor(self.targets[index], dtype=torch.float),
+        }
+
+
 
 def get_categories():
     with open("observation_categories.json", "r") as f:

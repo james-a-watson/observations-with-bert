00e97eb03ce7502637a1f43f8b21af012c97f747
James Watson <jameswatson16@hotmail.com>
2023-10-09 10:49:42 +0100

    Add forward

--- model.py
+++ model.py
@@ -7,6 +7,12 @@ class BERTClass(torch.nn.module):
         self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased', return_dict=False)
         self.l2 = torch.nn.Dropout(0.3)
         self.l3 = torch.nn.Linear(768, 29)
+
+    def forward(self, ids, mask, token_type_ids):
+        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
     
 def load_ckp(): 
     raise NotImplementedError()

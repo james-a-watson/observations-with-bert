07a578883873ea377bb3ad317fa4fdee2a530428
James Watson <jameswatson16@hotmail.com>
2023-10-09 10:48:50 +0100

    start BERT Class

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
+
+    
+class BERTClass(torch.nn.module):
+    def __init__(self):
+        super(BERTClass, self).__init__()
+        self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased', return_dict=False)
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, 29)
+    
 def load_ckp(): 
     raise NotImplementedError()
-    
-def BERTClass(): 
-    raise NotImplementedError()
-    
+
 def train_model():
     raise NotImplementedError()

6c63ec867e6c0d076c308c332150af97ee244e55
James Watson <jameswatson16@hotmail.com>
2023-10-10 12:19:23 +0100

    Adding Model

--- model.py
+++ model.py
@@ -22,6 +22,92 @@ def load_checkpoint(checkpoint_fpath, model, optimizer):
     valid_loss_min = checkpoint["valid_loss_min"]
     return model, optimizer, checkpoint["epoch"], valid_loss_min
 
+def train_model(
+        start_epochs,
+        n_epochs,
+        valid_loss_min_input,
+        training_loader,
+        validation_loader,
+        model,
+        optimizer,
+        checkpoint_path,
+        best_model_path
+):
+    # Initiialize valid loss minimum at input.
+    valid_loss_min = valid_loss_min_input
 
-def train_model():
-    raise NotImplementedError()
+    for epoch in range(start_epochs, n_epochs):
+        train_loss = 0
+        valid_loss = 0
+        # Put model in training mode.
+        model.train()
+
+        print(f" -- Epoch {epoch}: Training Start -- ")
+        
+        for batch_idx, data in enumerate(training_loader):
+            # Save batch info to device
+            ids = data["ids"].to(device, dtype=torch.long)
+            mask = data["mask"].to(device, dtype=torch.long)
+            token_type_ids = data["token_type_ids"].to(device, dtype=torch.long)
+            targets = data["targets"].to(device, dtype=torch.float)
+            # Run prediction on model for batch
+            outputs = model(ids, mask, token_type_ids)
+            optimizer.zero_grad()
+            # Evaluate loss
+            loss = loss_fn(outputs, targets)
+            
+            if batch_idx%5000 == 0:
+                print(f"Epoch: {epoch}, Training Loss: {loss.item()}")
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss += (1 / (batch_idx + 1))*(loss.item() - train_loss)
+        
+        print(f" -- Epoch {epoch}: Training End -- ")
+
+        print(f" -- Epoch {epoch}: Validation Start -- ")
+
+        model.eval()ßß
+
+        with torch.no_grad():
+            val_targets = []
+            val_outputs = []
+            for batch_idx, data in enumerate(training_loader):
+                # Save batch info to device
+                ids = data["ids"].to(device, dtype=torch.long)
+                mask = data["mask"].to(device, dtype=torch.long)
+                token_type_ids = data["token_type_ids"].to(device, dtype=torch.long)
+                targets = data["targets"].to(device, dtype=torch.float)
+                # Evalutate model on batch
+                outputs = model(ids, mask, token_type_ids)
+
+                loss = loss_fn(outputs, targets)
+                valid_loss += (1 / (batch_idx + 1))*(loss.item() - valid_loss)
+                val_targets.extend(targets.cpu().detach().numpy().tolist())
+                val_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+
+        print(f" -- Epoch {epoch}: Validation End --")
+
+        train_loss = train_loss/len(training_loader)
+        valid_loss = valid_loss/len(validation_loader)
+
+        print(f"Epoch: {epoch}\n\tAverage Training Loss: {train_loss}\n\tAverage Validation Loss: {valid_loss}")
+
+        checkpoint = {
+            "epoch": epoch + 1, 
+            "valid_loss_min": valid_loss,
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict()
+        }
+
+        save_checkpoint(checkpoint, False, checkpoint_path, best_model_path)
+
+        if valid_loss <= valid_loss_min:
+            print(f"Validation loss decreased ({valid_loss_min} --> {valid_loss}). Saving Model...")
+            save_checkpoint(checkpoint, True, checkpoint_path, best_model_path)
+            valid_loss_min = valid_loss
+
+        print(f" -- Epoch {epoch} Done -- ")
+    
+    return model
